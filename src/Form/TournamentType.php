ee8e38d131ae1103f79328cc6243414d14837664
stephchris <estom35@gmail.com>
2023-03-03 15:05:41 +0100

    1ère étape to upload file

--- src/Form/TournamentType.php
+++ src/Form/TournamentType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Tournament;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class TournamentType extends AbstractType
 {
@@ -19,7 +21,31 @@ class TournamentType extends AbstractType
             ->add('type')
             ->add('playerNumber')
             ->add('tableNumber')
-            ->add('image')
+            ->add('photo', FileType::class, [
+                'label' => 'Illustration ()',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/gif',
+                            'image/webp',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image',
+                    ])
+                ],
+            ])
         ;
     }
 
